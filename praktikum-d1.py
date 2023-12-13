06de887c8e1710fb03a19d2ba3f3dadcb0c4d554
Dwickiherlambang <152823995+Dwickiherlambang@users.noreply.github.com>
2023-12-13 14:30:38 +0800

    Upload file 2

---
+++ praktikum-d1.py
@@ -0,0 +1,243 @@
+print()
+print('\tpraktikum-d1')
+print()
+print('Nama Lengkap  = Dwicki Herlambang')
+print('NIM           = 231031113')
+print('Prodi         = Sistem Informasi')
+Angkatan = 2023
+print('Angkatan Adalah',Angkatan)
+print()
+print('\tTipe Data')
+print()
+a=20
+print("Data adalah =",a)
+print('Tipenya Adalah =',type(a))
+print()
+b=20.0
+print("Data adalah =",b)
+print('Tipenya Adalah =',type(b))
+print()
+c=20j
+print("Data adalah =",c)
+print('Tipenya Adalah =',type(c))
+print()
+kampus = 'Institut Teknologi BJ Habibie'
+print("Data Adalah =",type(kampus))
+print()
+log = True
+print('Data Adalah =',log)
+print('Tipenya Adalah =',type(log))
+print()
+C= complex(20,8)
+print('Data Adalah =',C)
+print('Tipenya Adalah =',type(C))
+print()
+
+print('\tOperator')
+print()
+p=20
+q=5
+Hasil = p+q
+print("Hasil",p,'+',q,'Adalah',Hasil)
+print()
+p=20
+q=5
+Hasil = p-q
+print("Hasil",p,'-',q,'Adalah',Hasil)
+print()
+p=20
+q=5
+Hasil = p*q
+print("Hasil",p,'x',q,'Adalah',Hasil)
+print()
+p=20
+q=3
+Hasil = p/q
+print("Hasil",p,':',q,'Adalah',Hasil)
+print()
+p=3
+q=2
+Hasil = p**q
+print("Hasil",p,'^',q,'Adalah',Hasil)
+print()
+p=37
+q=31
+Hasil = p%q
+print("Hasil",p,'%',q,'Adalah',Hasil)
+print()
+p=37
+q=31
+Hasil = p//q
+print("Hasil",p,'//',q,'Adalah',Hasil)
+print()
+a=20
+print("Nilai a Adalah", a)
+a +=1                                #ini perintah a= a+1
+print("Nilai a+=1 Menjadi", a)
+print()
+a=25
+print("Nilai a Adalah", a)
+a -=2                #ini perintah a= a-2
+print("Nilai a-=2 Menjadi", a)
+print()
+a=20
+print("Nilai a Adalah", a)
+a *=2                #ini perintah a= a*2
+print("Nilai a*=2 Menjadi", a)
+print()
+a=20
+print("Nilai a Adalah", a)
+a /=5                #ini perintah a= a/5
+print("Nilai a/=2 Menjadi", a)
+print()
+a=20
+print("Nilai a Adalah", a)
+a **=2                #ini perintah a= a**2
+print("Nilai a^2 Menjadi", a)
+print()
+a=20
+print("Nilai a Adalah", a)
+a %=7                #ini perintah a= a%7
+print("Nilai a%=2 Menjadi", a)
+print()
+a=20
+print("Nilai a Adalah", a)
+a //=7                #ini perintah a= a//7
+print("Nilai a//=2 Menjadi", a)
+print()
+print() #ini operasi OR
+log=True
+print("Nilai log Adalah",log)
+log |= False        #ini perintah log= False
+print("Nilai log|=False menjadi",log)
+print()
+print() #ini operasi AND
+log=True
+print("Nilai log Adalah",log)
+log &= True        #ini perintah log= log & False
+print("Nilai log|=True menjadi",log)
+print()
+print() #ini operasi XOR
+log=True
+print("Nilai log Adalah",log)
+log ^= True        #ini perintah log= log ^ True
+print("Nilai log|=True menjadi",log)
+
+print() #ini operasi shifting
+bi =0b0100
+print("Nilai bi =",format(bi,"04b"))
+bi >>=1
+print('Nilai bi >>=1 menjadi',format(bi,"04b"))
+
+print() #ini operasi shifting
+bi =0b0100
+print("Nilai bi =",format(bi,"04b"))
+bi <<=1
+print('Nilai bi >>=1 menjadi',format(bi,"04b"))
+print()
+print('\t\tTugas') 
+print('\tOperator Komperasi')
+print()
+
+a=165
+b=31
+c=134
+d=9321
+print('Diketahui Bilangan\n a =',a,'\n b =',b,'\n c =',c,'\n d =',d)
+print()
+
+hasil=a<b
+print('Perbadingan antara a<b adalah',hasil)
+
+hasil=a>b
+print('Perbadingan antara a>b adalah',hasil)
+
+hasil=a<b>c
+print('Perbadingan antara a<b>c adalah',hasil)
+
+hasil=a>b<c
+print('Perbadingan antara a<b>c adalah',hasil)
+
+hasil=a>b<c
+print('Perbadingan antara a<b>c adalah',hasil)
+
+hasil=a>b<c
+print('Perbadingan antara a<b>c adalah',hasil)
+
+hasil=a*b//2 > d
+print('Perbadingan antara axb/2 > c adalah',hasil)
+
+hasil=a*b**a == 18
+print('Perbadingan antara a*b^a = 18 adalah',hasil)
+
+hasil=a and b < d
+print('Perbadingan antara a dan b < d adalah',hasil)
+
+hasil=a is not b
+print('Perbadingan antara a yang bukan b adalah',hasil)
+
+hasil=a is not b
+print('Perbadingan antara a yang bukan b adalah',hasil)
+
+hasil=a*b is 6
+print('Perbadingan antara axb adalah 6 ',hasil)
+
+hasil=a*b//c is not 10
+print('Perbadingan antara axb/c yang bukan 10 ',hasil)
+
+hasil= c + b == b
+print(hasil,'Karena hasil dari c + b =',c + b,'tidak sama dengan bilangan b yaitu',b)
+print('Apabila c + b =',c+b,'jika dihubungkan dengan bilangan a hasilnya adalah',"'",c+b==a,"'",'karena nilai c+b=',c+b,'itu sama dengan',a)
+print()
+a='apakah'
+b='dia'
+c='punya'
+d='saya'
+e='boleh'
+f='mau'
+g='memiliki'
+hasil=a is 'aku'
+
+print('a adalah "aku"',hasil)
+
+print('a tidak "aku"',a is not 'aku')
+
+print(a,b,c,d,'Ternyata', b is c)
+print(a,e,d,g,b,a==c+d)
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
