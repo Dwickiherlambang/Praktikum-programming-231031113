3bbcfb440094bc47e6a503f2e0602c774b969958
Dwickiherlambang <152823995+Dwickiherlambang@users.noreply.github.com>
2023-12-14 22:56:14 +0800

    Upload File 5

---
+++ praktikum-d5.py
@@ -0,0 +1,10 @@
+a = True
+i = 0
+batas = 10
+while a :
+    i += 1
+    if  i <= batas :
+        print('Selamat Bergabung',batas+ 1 -i)
+    else:
+        a = False  
+        print('Program Berhenti Disini')
